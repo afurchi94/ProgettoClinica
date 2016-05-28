849db7269bc1d7bd33796cc94765bf29f1c63568
IronAx3 <c.fabio94@gmail.com>
2016-05-28 15:21:24 +0200

    ho adattato il file alle tue modifiche

--- src/projectX/Scuola.java
+++ src/projectX/Scuola.java
@@ -1,5 +1,11 @@
 package projectX;
 
 public class Scuola {
-private String indirizzo;
+	private String indirizzo;
+	private String nome;
+
+	public Scuola(String nome) {
+		this.nome = nome;
+	}
+
 }
